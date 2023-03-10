3915a1f21e00db1c58ff6b1a1aed20171cca998f
naji <xx@zz>
2023-03-10 21:22:50 +0300

    created signUpCharity on sign up

---
+++ lib/sign up/signUpCharity.dart
@@ -0,0 +1,28 @@
+import 'package:demo2/log%20in/rememberMe.dart';
+import 'package:flutter/material.dart';
+
+
+class signUpChartiy extends StatelessWidget {
+  const signUpChartiy
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+   children:const[
+      TextField(
+      decoration: InputDecoration(
+        labelText:'Charity Name'
+        
+      ),
+
+    ),
+    TextField(
+
+    ),
+
+
+   ]
+    );
+  }
+}
