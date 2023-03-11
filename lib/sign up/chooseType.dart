1d6d5c76e33e1561b3135fa50ab81aa891c34c31
ABUHAMRA <102425196+ABUHAMRA@users.noreply.github.com>
2023-03-12 00:13:38 +0300

    add pic to choose user

--- lib/sign up/chooseType.dart
+++ lib/sign up/chooseType.dart
@@ -8,57 +8,65 @@ class Choose extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'choose account type',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  decoration: TextDecoration.none),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            // BoxDecoration takes the image
+            decoration: BoxDecoration(
+              // Image set to background of the body
+              image: DecorationImage(
+                  image: AssetImage("assets/background.jpg"), fit: BoxFit.fill),
             ),
-            SizedBox(height: 30),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUp()),
-                  );
-                },
-                child: Text('user', style: TextStyle(color: Colors.black)),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-                  backgroundColor: MaterialStateProperty.all(logoColor),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'choose account type',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: null,
-                child:
-                    Text('organization', style: TextStyle(color: Colors.black)),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-                  backgroundColor: MaterialStateProperty.all(logoColor),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    child: Text('user', style: TextStyle(color: Colors.black)),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                      backgroundColor: MaterialStateProperty.all(logoColor),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(height: 10),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    child: Text('organization',
+                        style: TextStyle(color: Colors.black)),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                      backgroundColor: MaterialStateProperty.all(logoColor),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
